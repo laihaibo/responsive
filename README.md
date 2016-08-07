e36da3737451c84ae03afc8d0b31286873c2f9d5
laihaibo <18768101327@163.com>
2016-08-07 23:32:29 +0800

    add responsive

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# responsive
-a responsive web page
+一个响应式的互联网金融网站
+
+### version
+1.0.0
+
+### feature
+* 兼容IE9及以上的现代浏览器
+* SEO优化
+* html5语义化
+* 页面结构优化
+* gulp自动化构建
+* 图片资源优化，css sprite，png压缩
---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+/*
+* @Author: laihaibo
+* @Date:   2016-08-02 14:02:22
+* @Last Modified by:   laihaibo
+* @Last Modified time: 2016-08-06 23:07:41
+*/
+
+'use strict';
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+  });
+})
