94e23b7a13bf35220f55ea560a9adf9aeb6ccdad
laihaibo <18768101327@163.com>
2016-08-13 22:58:10 +0800

    fixup
    1.整理代码
    2.添加信息滚动条功能

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,21 @@ $(document).ready(function(){
     autoplayTimeout:3000,
     autoplayHoverPause:true,
   });
+  var $scrollInfo = $('#scrollInfo');
+  var timer;
+  var speed = 50;
+  var daly = 2000;
+  console.log(new Date());
+  function start() {
+    $('#scrollInfo li:first').slideUp().queue(function (next) {
+      $(this).appendTo($scrollInfo);
+      next();
+    }).slideDown();
+    timer = setInterval(scroll, speed);
+  }
+  function scroll() {
+    clearInterval(timer);
+    setTimeout(start, daly);
+  }
+  setTimeout(start, daly);
 })
